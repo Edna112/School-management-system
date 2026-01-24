3f2138324fdc68088c65b73bf2113a1d71dc9c83
Edna112 <chenwieddy@gmail.com>
2026-01-23 16:11:00 -0800

    implement data provider

---
+++ src/constants/index.tsx
@@ -0,0 +1,20 @@
+export const DEPARTMENTS= ['CS', 
+    'Math', 
+    'English',
+    'Physics',
+    'Chemistry',
+    'Biology',
+    'History',
+    'Geography',
+    'Economics',
+    'Political Science',
+    'Sociology',
+    'Philosophy',
+    'Art',
+    'Music',
+    'Dance'];
+
+    export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((department) => ({
+        label: department,
+        value: department,
+    }));
